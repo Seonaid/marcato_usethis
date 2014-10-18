13b8457f4b5fcc236ad7b4564f9638a1e6180532
Seonaid <seonaidl@gmail.com>
2014-10-18 11:32:58 -0300

    creating login html and filling in bones

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use("/assets", express.static(__dirname + '/assets'));
+
+http.listen(8080, function(){
+	console.log('listening on *:8080');
+});
