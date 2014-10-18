c2574fcb8f8be6629d91ef3c1e843be85536b221
Sean P. O. MacCath-Moran <smaccathmoran@phase2technology.com>
2014-10-18 19:02:47 -0300

    adding inventory screen

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 var express = require('express');
 var session = require('cookie-session');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 var app = express();
 var http = require('http').Server(app);
 
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -19,61 +14,16 @@ app.use("/assets", express.static(__dirname + '/assets'));
 app.use('/', express.static(__dirname + '/public'));
 
 // process the login form
-app.post('/login', passport.authenticate('local-login', {
-  successRedirect : '/login-success.html',
-  failureRedirect : '/login-fail.html'
-}));
+app.post('/login', function(req, res){
+  
+});
 
-
-// set up our express application
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
-// required for passport
-app.use(session({ secret: 'marcatohackathon' })); // session secret
-
-// passport.use('local-login', new LocalStrategy({
-//   // by default, local strategy uses username and password, we will override with email
-//   usernameField : 'email',
-//   passwordField : 'password',
-//   passReqToCallback : true // allows us to pass back the entire request to the callback
-// },
-// function(req, email, password, done) { // callback with email and password from our form
-//   // find a user whose email is the same as the forms email
-//   // we are checking to see if the user trying to login already exists
-//   
-//   if ('sean' == email) {
-//     return done(null, user);
-//     
-//   }
-// //   User.findOne({ 'local.email' : email }, function(err, user) {
-// //     // if there are any errors, return the error before anything else
-// //     if (err)
-// //     return done(err);
-// //     // if no user is found, return the message
-// //     if (!user)
-// //   //   return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-// //     return done(null, false, false); // req.flash is the way to set flashdata using connect-flash
-// //     // if the user is found but the password is wrong
-// //     if (!user.validPassword(password))
-// //   //   return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
-// //     return done(null, false, false); // create the loginMessage and save it to session as flashdata
-// //     // all is well, return successful user
-// //     return done(null, user);
-// //   });
-// }));
-
-passport.use('local-login', new LocalStrategy(function(username, password, done) {
-  process.nextTick(function() {
-    if ('sean' == username) {
-      return done(null, username);
-    }
-    else {
-      return done(null, false);
-    }
-  });
-}));
+// process the addinventory form
+app.post('/add-inventory', function(req, res){
+  
+});
 
 
 http.listen(8080, function(){
-	console.log('listening on *:8080');
+  console.log('listening on *:8080');
 });
